a3f3bee1996b1d1b59e9a7d7060d1177c5de7cda
Sahil Yeole <sahilyeole93@gmail.com>
2024-07-04 10:26:10 +0530

    update bench
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- k6/bench.sh
+++ k6/bench.sh
@@ -2,6 +2,7 @@ test_name=$1
 benchmark=$2
 
 if [ "$IS_PUSH" = "true" ]; then
+  echo "Cloud enabled"
   k6 run k6/bench.js --quiet --out cloud --env TEST_NAME=$test_name --env BENCHMARK=$benchmark
 else
     k6 run k6/bench.js --quiet --env TEST_NAME=$test_name --env BENCHMARK=$benchmark
