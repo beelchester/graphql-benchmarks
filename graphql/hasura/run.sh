47ca48b8e97cdf31b365cff7bd2ddb458a016a1c
Sahil Yeole <sahilyeole93@gmail.com>
2024-07-08 12:13:32 +0530

    temp
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- graphql/hasura/run.sh
+++ graphql/hasura/run.sh
@@ -26,7 +26,7 @@ echo "PostgreSQL is ready!"
 echo "$(pwd)/graphql/hasura"
 ls -la "$(pwd)/graphql/hasura"
 
-mkdir -p /Users/$(whoami)/hasura
+mkdir -p "/Users/$(whoami)/hasura"
 cp -r "$(pwd)/graphql/hasura" "/Users/$(whoami)/hasura"
 ls -la "/Users/$(whoami)/hasura"
 
